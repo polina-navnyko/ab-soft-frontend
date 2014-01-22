64e2683334acc0454642b246a9d8ecaee92c25e6
Polina Navnyko <polyna_navnyko@tut.by>
2014-01-22 21:36:21 +0200

    -Shadow, header styles are modified; -Add some images -Product's pages except 1394, imagewarp -mediaqueries to products well done

---
+++ frontend/js/menu/functions.js
@@ -0,0 +1,14 @@
+$(function() {
+	$("a").bind('click',function() {
+		var _this = $(this);
+		
+		// Раскрываем текущую ссылку 
+		_this.toggleClass('selected', 5);
+		_this.next().toggleClass('dropdown', 500);
+		// Проходим по другим ссылкам и выключаем активное состояние
+		$("a").not(_this).each(function() {
+			$(this).next().addClass('dropdown', 500);
+			$(this).removeClass('selected', 5);
+		});
+	});
+}); 
